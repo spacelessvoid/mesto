3a526cd72ac125ae697d1dfa163d68cfd252498a
Aleksey Tishenkov <tishenkov@gmail.com>
2023-01-25 11:34:30 +0400

    fix: fix js and css acc. to spec after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,34 @@
-// Закрытие и открытие попапа
-
 let popupCloseBtn = document.querySelector(".popup__close");
 let profileEditBtn = document.querySelector(".profile__edit-btn");
 let popupOverlay = document.querySelector(".popup");
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__text-input_type_name");
+let jobInput = formElement.querySelector(".popup__text-input_type_job");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
 
 function popupToggle() {
   popupOverlay.classList.toggle("popup_opened");
 }
 
-profileEditBtn.addEventListener("click", popupToggle);
-popupCloseBtn.addEventListener("click", popupToggle);
+profileEditBtn.addEventListener("click", () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 
-let formElement = document.querySelector(".popup__container");
-let nameInput = formElement.querySelector(".popup__text-input_name");
-let jobInput = formElement.querySelector(".popup__text-input_job");
+  popupToggle();
+});
+
+popupCloseBtn.addEventListener("click", popupToggle);
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  // Получите значение полей jobInput и nameInput из свойства value
   let name = nameInput.value;
   let job = jobInput.value;
 
-  // Выберите элементы, куда должны быть вставлены значения полей
   let profileName = document.querySelector(".profile__name");
   let profileJob = document.querySelector(".profile__job");
 
-  // Вставьте новые значения с помощью textContent
   profileName.textContent = name;
   profileJob.textContent = job;
 
