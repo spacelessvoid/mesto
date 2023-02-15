9cbf78cd6f74327809607ae15fa3f27a37428aa0
Aleksey Tishenkov <tishenkov@gmail.com>
2023-02-15 21:46:27 +0400

    feat: add image add function refactor: improved modal window code

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,113 @@
-let popupCloseBtn = document.querySelector(".popup__close");
-let profileEditBtn = document.querySelector(".profile__edit-btn");
-let popupOverlay = document.querySelector(".popup");
-let popupContainer = document.querySelector(".popup__container");
-let formElement = document.querySelector(".popup__form");
-let nameInput = formElement.querySelector(".popup__text-input_type_name");
-let jobInput = formElement.querySelector(".popup__text-input_type_job");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
-function popupToggle() {
-  popupOverlay.classList.toggle("popup_opened");
+const templateCard = document.querySelector("#card");
+const templatePopupEditProfile = document.querySelector("#popup-edit-profile");
+const templatePopupAddImage = document.querySelector("#popup-add-image");
+const page = document.querySelector(".page");
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+
+page.addEventListener("click", (evt) => {
+  if (
+    // Closing popup
+    evt.target.classList.contains("popup__close") ||
+    evt.target.classList.contains("popup")
+  ) {
+    handlePopupClose(evt);
+  } else if (evt.target.classList.contains("profile__edit-btn")) {
+    // Opening profile edit popup
+    openEditProfilePopup();
+  } else if (evt.target.classList.contains("add-btn")) {
+    openAddImagePopup();
+  }
+});
+
+function handleAddImage(name, link) {
+  const newCard = templateCard.content.cloneNode(true);
+  const title = newCard.querySelector(".card__place");
+  const image = newCard.querySelector(".card__image");
+  title.textContent = name;
+  image.alt = "Фото: " + name;
+  image.src = link;
+  document.querySelector(".gallery").prepend(newCard);
 }
 
-function popupClose() {
+initialCards.forEach((el) => {
+  handleAddImage(el.name, el.link);
+});
+
+function handlePopupClose(evt) {
+  const popupOverlay = document.querySelector(".popup");
+  const popupContainer = document.querySelector(".popup__container");
   popupOverlay.classList.add("popup_animation_fade-out");
   popupContainer.classList.add("popup__container_animation_slide-out");
   setTimeout(() => {
-    popupToggle();
+    evt.target.closest(".popup").remove();
     popupOverlay.classList.remove("popup_animation_fade-out");
     popupContainer.classList.remove("popup__container_animation_slide-out");
   }, 550);
 }
 
-// Opening popup
-profileEditBtn.addEventListener("click", () => {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+function openAddImagePopup() {
+  const popupItem = templatePopupAddImage.content.cloneNode(true);
+  page.append(popupItem);
+  document.querySelector(".popup").classList.add("popup_opened");
+}
 
-  popupToggle();
-});
+function openEditProfilePopup() {
+  const popupItem = templatePopupEditProfile.content.cloneNode(true);
+  page.append(popupItem);
+  document.querySelector(".popup").classList.add("popup_opened");
 
-// Pressing X to close popup
-popupCloseBtn.addEventListener("click", popupClose);
+  document.querySelector(".popup__text-input_type_name").value =
+    profileName.textContent;
+  document.querySelector(".popup__text-input_type_job").value =
+    profileJob.textContent;
+}
 
-// Pressing outside popup to close it
-popupOverlay.addEventListener("click", (event) => {
-  if (event.target === event.currentTarget) {
-    popupClose();
-  }
-});
-
-// Submitting the form in the popup
+// Submitting the form
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  let name = nameInput.value;
-  let job = jobInput.value;
+  let name = document.querySelector(".popup__text-input_type_name");
+  let job = document.querySelector(".popup__text-input_type_job");
+  let link = document.querySelector(".popup__text-input_type_link");
+  // Submitting the profile edit popup
+  if (evt.target.classList.contains("form__edit-profile")) {
+    profileName.textContent = name.value;
+    profileJob.textContent = job.value;
+  }
+  // Submitting the add image popup
+  if (evt.target.classList.contains("form__add-image")) {
+    handleAddImage(name.value, link.value);
+  }
 
-  let profileName = document.querySelector(".profile__name");
-  let profileJob = document.querySelector(".profile__job");
-
-  profileName.textContent = name;
-  profileJob.textContent = job;
-
-  popupClose();
+  handlePopupClose(evt);
 }
 
-formElement.addEventListener("submit", handleFormSubmit);
+page.addEventListener("submit", handleFormSubmit);
